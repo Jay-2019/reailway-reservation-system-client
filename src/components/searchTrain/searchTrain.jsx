c0eb102481beea9415b1f9dcc288041372d1f6fe
jay <jay.mauryavanshi2014@gmail.com>
2020-02-03 10:58:13 +0530

    added a feature in  search train...

--- src/components/searchTrain/searchTrain.jsx
+++ src/components/searchTrain/searchTrain.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import UserNavigationBar from "../userNavigationBar/index";
+import axios from "axios";
 
 export default class SearchTrain extends Component {
   state = {
     from: "",
     to: "",
-    isStationNotSame: false
+    isStationNotSame: false,
+    getStation: []
   };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/railwayReservationSystem/getStation")
+      .then(response => this.setState({ getStation: response.data }))
+      .catch(err => console.log(err.response));
+  }
 
   handleFrom = event => {
     const { value } = event.target;
@@ -47,76 +56,73 @@ export default class SearchTrain extends Component {
       <div>
         <UserNavigationBar />
         <br />
-      
-      <div className="d-flex justify-content-center">
-        
-        <div className="card bg-light mb-3">
-          <div className="card-header">
-            <h3 className="d-flex justify-content-center">
-              Your Journey is Here
-            </h3>
-          </div>
-          <div className="card-body">
-            <h5 className="card-title">
-              <form onSubmit={this.handleSubmit}>
-                <div className="form-row">
-                  <div className="col">
-                    <div className="form-row">
-                      <div className="col">
-                        <label htmlFor="inputState">From</label>
-                        <select
-                          id="from"
-                          className="form-control"
-                          onChange={this.handleFrom}
-                          value={this.state.from}
-                          required
-                        >
-                          <option value="Select">Select</option>
-                          <option value="Delhi">Delhi</option>
-                          <option value="Kanpur">Kanpur</option>
-                          <option value="Chitrakoot">Chitrakoot</option>
-                          <option value="Lucknow">Lucknow</option>
-                          <option value="Allahabad">Allahabad</option>
-                          <option value="Gwalior">Gwalior</option>
-                        </select>
-                      </div>
 
-                      <div className="col">
-                        <label htmlFor="inputState">To</label>
-                        <select
-                          id="to"
-                          className="form-control"
-                          onChange={this.handleTo}
-                          value={this.state.to}
-                          required
-                        >
-                          <option value="Select">Select</option>
-                          <option value="Delhi">Delhi</option>
-                          <option value="Kanpur">Kanpur</option>
-                          <option value="Chitrakoot">Chitrakoot</option>
-                          <option value="Lucknow">Lucknow</option>
-                          <option value="Allahabad">Allahabad</option>
-                          <option value="Gwalior">Gwalior</option>
-                        </select>
+        <div className="d-flex justify-content-center">
+          <div className="card bg-light mb-3">
+            <div className="card-header">
+              <h3 className="d-flex justify-content-center">
+                Your Journey is Here
+              </h3>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-row">
+                    <div className="col">
+                      <div className="form-row">
+                        <div className="col">
+                          <label htmlFor="inputState">From</label>
+                          <select
+                            id="from"
+                            className="form-control"
+                            onChange={this.handleFrom}
+                            value={this.state.from}
+                            required
+                          >
+                            <option value="Select">Select</option>
+                            {this.state.getStation.map((station, key) => (
+                              <option key={key} value={station}>
+                                {station}
+                              </option>
+                            ))}
+                          </select>
+                        </div>
+
+                        <div className="col">
+                          <label htmlFor="inputState">To</label>
+                          <select
+                            id="to"
+                            className="form-control"
+                            onChange={this.handleTo}
+                            value={this.state.to}
+                            required
+                          >
+                            <option value="Select">Select</option>
+                            {this.state.getStation.map((station, key) => (
+                              <option key={key} value={station}>
+                                {station}
+                              </option>
+                            ))}
+                          </select>
+                        </div>
                       </div>
-                    </div>
-                    <br />
-                    <div>
-                      <button
-                        type="submit"
-                        value="createTicket"
-                        className="btn btn-dark btn-lg btn-block"
-                      >
-                        Search Train
-                      </button>
+                      <br />
+                      <div>
+                        <button
+                          type="submit"
+                          value="createTicket"
+                          className="btn btn-dark btn-lg btn-block"
+                        >
+                          Search Train
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </form>
-            </h5>
+                </form>
+              </h5>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }
